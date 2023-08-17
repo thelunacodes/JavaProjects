4641b6ff226a1262928fea5c72874ffd30eb332b
Gabriel Silveira de Luna <gabriel.luna@lab.infnet.com.br>
2023-08-17 12:20:29 -0300

    nada aconteceu

--- EN/IPAddressValidator.java
+++ EN/IPAddressValidator.java
@@ -12,34 +12,29 @@ public class IPAddressValidator {
         //User Input
         System.out.print("Insert an IP address: ");
         ip = scanner.nextLine();
-
         //Count dots in IP address
-        for (int i = 1; i <= ip.length(); i++) {
-            if (ip.charAt(i-1) == '.') {
+        for (int i = 0; i < ip.length(); i++) {
+            if (ip.charAt(i) == '.') {
                 dotCounter++;
             }
         }
-
         //Split IP address into array of octets
         octets = ip.split("\\.");
-
         //Check if IP address has 4 octets
         if (octets.length == 4) {
-
             //Check if every octet is bigger or equal to 0 AND smaller or equal to 255
             if ((Integer.parseInt(octets[0]) >= 0 && Integer.parseInt(octets[0]) <= 255) && (Integer.parseInt(octets[1]) >= 0 && Integer.parseInt(octets[1]) <= 255) && (Integer.parseInt(octets[2]) >= 0 && Integer.parseInt(octets[2]) <= 255) && (Integer.parseInt(octets[3]) >= 0 && Integer.parseInt(octets[3]) <= 255)) {
                 octetIsValid = true;
             } else {
-                System.out.println("INVALID IP: every octet has to have a value between 0 and 255.");
+                System.out.println("INVALID IP: Every octet has to have a value between 0 and 255.");
                 octetIsValid = false;
             }
         } else {
-            System.out.println("INVALID IP: every IP adress has to have four octets.");
+            System.out.println("INVALID IP: Every IP address has to have four octets.");
             octetIsValid = false;
         }  
-
         if (dotCounter == 3 && octetIsValid) {
-            System.out.printf("The ip %s is valid!", ip);
+            System.out.printf("The IP address %s is valid!", ip);
         }
         scanner.close();
     }
--- PTBR/ValidadorDeEndereçoIP.java
+++ PTBR/ValidadorDeEndereçoIP.java
@@ -13,32 +13,27 @@ public class ValidadorDeEndereçoIP {
         //Input do usuário
         System.out.print("Insira um endereço de IP: ");
         ip = scanner.nextLine();
-
         //Contar os "." no endereço de IP inserido
-        for (int i = 1; i <= ip.length(); i++) {
-            if (ip.charAt(i-1) == '.') {
+        for (int i = 0; i < ip.length(); i++) {
+            if (ip.charAt(i) == '.') {
                 contadorDePontos++;
             }
         }
-
         //Criar um array de octetos a partir do endereço de IP
         octetos = ip.split("\\.");
-
-        //checar se o endereço de IP tem quatro octetos
+        //Checar se o endereço de IP tem quatro octetos
         if (octetos.length == 4) {
-
             //Checar se todos os octetos são maiores ou iguais a 0 E menores ou iguais a 255
             if ((Integer.parseInt(octetos[0]) >= 0 && Integer.parseInt(octetos[0]) <= 255) && (Integer.parseInt(octetos[1]) >= 0 && Integer.parseInt(octetos[1]) <= 255) && (Integer.parseInt(octetos[2]) >= 0 && Integer.parseInt(octetos[2]) <= 255) && (Integer.parseInt(octetos[3]) >= 0 && Integer.parseInt(octetos[3]) <= 255)) {
                 octetoEhValido = true;
             } else {
-                System.out.println("IP INVÁLIDO: todos os octetos devem estar entre 0 e 255.");
+                System.out.println("IP INVÁLIDO: Todos os octetos devem estar entre 0 e 255.");
                 octetoEhValido = false;
             }
         } else {
-            System.out.println("IP INVÁLIDO: o endereço de IP deve ter quatro octetos.");
+            System.out.println("IP INVÁLIDO: O endereço de IP deve ter quatro octetos.");
             octetoEhValido = false;
         }  
-
         if (contadorDePontos == 3 && octetoEhValido) {
             System.out.printf("O IP %s é um endereço válido!", ip);
         } 
