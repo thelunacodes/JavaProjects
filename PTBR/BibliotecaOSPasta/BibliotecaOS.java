af3aab228af77c62ba8317e6b7669c9f7512e033
delunatriestocode <delunacomunicacao@gmail.com>
2023-08-21 18:37:38 -0300

    projeto bibliotecaOS dia 1

--- PTBR/BibliotecaOSPasta/BibliotecaOS.java
+++ PTBR/BibliotecaOSPasta/BibliotecaOS.java
@@ -6,42 +6,49 @@ import java.util.Scanner;
 
 public class BibliotecaOS {
     public static void main(String[] args) {
+        //atributos gerais
         Scanner scanner = new Scanner(System.in);
         String nomeLivro = "", autor = "", editora = "", CDD = "", ISBN = "", secao = "";
-        int edicao = 0, anoPublicacao = 0, prateleira = 0, opcao = 0;
+        int edicao = 0, anoPublicacao = 0, prateleira = 0, opcao1 = 0;
         ArrayList<Livro> listaLivros = new ArrayList<Livro>();
 
-        // LEMBRETES:
+        //LEMBRETES:
         //
-        // 1- Navegação da lista de livros
-        //
-
+        //1- Resolver bug do case 3
 
         //Navegação do App
         do {
-            switch (opcao) {
+            switch (opcao1) {
                 //Intro
-                case 0:      
-                    System.out.println("\n===================================\n\tCadastro De Livros\n\t\tv1.0\n===================================");
+                case 0:
+                    //ArrayList de teste
+                    final int placeholderListaTamanho = 1000;
+                    for (int i = 0; i < placeholderListaTamanho; i++) {
+                        listaLivros.add(new Livro("aaa" + i, "aaaaaa", "aaaa", "aaaa", "aaaa", 2, 2003, "aaaa", 3));      
+                    }
+
+                    System.out.println("\n============================\n\tBibliotecaOS\n\t    v1.0\n============================");
                     System.out.println("\nBem-vindo(a), usuário(a)! Insira o número correspondente à opção desejada:");
                     //Opções
-                    System.out.println("\n[1] Cadastrar\n[2] Procurar pro palavra-chave\n[3] Listar livros\n[4] Espaço livre/usado\n[5] Excluir último elemento\n[6] Ordenar livros cadastrados\n[7] Sair\n");
-                    //Input
+                    System.out.println("\n[1] Cadastrar\n[2] Procurar por palavra-chave\n[3] Listar livros\n[4] Espaço livre/usado\n[5] Excluir último elemento\n[6] Ordenar livros cadastrados\n[7] Sair\n");
                     do {
+                        //Input
                         try {
                             System.out.print("> ");
-                            opcao = scanner.nextInt();
+                            opcao1 = scanner.nextInt();
                             break;
                         }
+                        //Se o usuário ousar inserir algo que não é número
                         catch(InputMismatchException e) {
                             System.out.println("ERRO: Insira apenas números inteiros, por favor.");
                             scanner.next();
                         }
                     } while (true);
-                    scanner.nextLine();
+                    scanner.nextLine(); //limpar buffer do scanner
                     break;
                 case 1:
                     if (listaLivros.size() < 1000) {
+                        System.out.println("\n===================================\n\tCadastro de Livros\n===================================\n");
                         //Nome
                         do {
                             System.out.print("Insira o nome do livro: ");
@@ -92,6 +99,8 @@ public class BibliotecaOS {
                             }
                             catch (InputMismatchException e) {
                                 System.out.println("ERRO: Apenas números, por favor.");
+                                scanner.next();
+
                             }
                         } while(true);
                         //Ano de Publicação
@@ -103,20 +112,16 @@ public class BibliotecaOS {
                             }
                             catch (InputMismatchException e) {
                                 System.out.println("ERRO: Apenas números, por favor.");
+                                scanner.next();
                             }
                         } while(true);
                         //Seção
                         do {
-                            try {
-                                scanner.nextLine();
-                                System.out.print("Insira o nome da seção onde está o livro: ");
-                                secao = scanner.next();
-                                if (!secao.trim().isEmpty()) {
-                                break;
-                                }
-                            }
-                            catch (InputMismatchException e) {
-                                System.out.println("ERRO: Apenas números, por favor.");
+                            scanner.nextLine();
+                            System.out.print("Insira o nome da seção onde está o livro: ");
+                            secao = scanner.next();
+                            if (!secao.trim().isEmpty()) {
+                            break;
                             }
                         } while(true);
                         //Prateleira
@@ -128,41 +133,104 @@ public class BibliotecaOS {
                             }
                             catch (InputMismatchException e) {
                                 System.out.println("ERRO: Apenas números, por favor.");
+                                scanner.next();
                             }
                         } while(true);
                         //Adicionar à lista
                         listaLivros.add(new Livro(nomeLivro, autor, editora, CDD, ISBN, edicao, anoPublicacao, secao, prateleira));
-                        opcao = 0;
+                        opcao1 = 0;
                     } else {
                        System.out.println("ERRO: A lista está cheia!");
-                       opcao = 0;
+                       esperar(1500);
+                       opcao1 = 0;
                     }
                     break;
                 case 3:
                     if (listaLivros.size() == 0) {
                         System.out.println("Não há nenhum livro cadastrado!");
-                        esperar(2000);
-                        opcao = 0;
+                        esperar(1500);
+                        opcao1 = 0;
                     } else {
-                        for (int i = 0; i < listaLivros.size(); i++) {
-                            System.out.printf("==================================================\n\t\t[Livro %d#]\n\nNome do Livro: %s\nAutor: %s\nEditora: %s\nCDD: %s\nISBN: %s\nEdição: %d°\nAno de Publicação: %d\nSeção: %s\nPrateleira: %d",i,nomeLivro,autor,editora,CDD,ISBN,edicao,anoPublicacao,secao,prateleira);
-                        }
-                        System.out.println();
-                        opcao = 0;
+                        int min_index = 0;
+                        int max_index = 4;
+                        int index = 0;
+                        String opcao2 = "";
+                        do {
+                            System.out.println("\n===============================\n\tLista de Livros\n===============================\n");
+                            for (int i = min_index; i <= max_index; i++) {
+                                try {
+                                    System.out.printf("[%d] - %s\n", i, listaLivros.get(i).getNomeLivro());
+                                }
+                                catch (IndexOutOfBoundsException e) {
+                                    break;
+                                }
+                            }
+                            //Introdução + primeira página da lista
+                            System.out.println("\nInsira... \n\n...\"I\" para acessar as informações de um livro pelo ID.\n...\"A\" para ir para a página anterior (se possível).\n...\"D\" para ir para a próxima página\n...ou \"S\" para voltar ao menu principal");
+                            System.out.print("\n> ");
+                            opcao2 = scanner.next();
+                            if (opcao2.toUpperCase().trim().equals("D")) {
+                                //PRÓXIMA PÁGINA
+                                if (listaLivros.size() > max_index) {
+                                    System.out.println(min_index);
+                                    System.out.println(max_index);
+                                    min_index += 5;
+                                    max_index += 5;
+                                    for (int i = min_index; i <= max_index; i++) {
+                                        try {
+                                            System.out.printf("[%d] - %s\n", i, listaLivros.get(i).getNomeLivro());
+                                        }
+                                        catch (IndexOutOfBoundsException e) {
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    System.out.println("ERRO: Não foi possível ir para a próxima página.");
+                                }
+                            } else if (opcao2.toUpperCase().trim().equals("A")) {
+                                //PRÓXIMA PÁGINA
+                                if (min_index > 0) {
+                                    System.out.println(min_index);
+                                    System.out.println(max_index);
+                                    min_index -= 5;
+                                    max_index -= 5;
+                                    for (int i = min_index; i <= max_index; i++) {
+                                        try {
+                                            System.out.printf("[%d] - %s\n", i, listaLivros.get(i).getNomeLivro());
+                                        }
+                                        catch (IndexOutOfBoundsException e) {
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    System.out.println("ERRO: Você já está na primeira página!");
+                                }
+                            } else if (opcao2.toUpperCase().trim().equals("S")) {
+                                break;
+                            } else {
+                                System.out.println("ERRO: Opção inválida!");
+                            }
+                        } while (true);
                     }
+                    opcao1 = 0;
                     break;
                 case 7:
+                    //Se o usuário tiver escolhido sair
                     break;
                 default:
+                    //Se o usuário tiver escolhido um número abaixo de 0 ou maior que 7
                     System.out.println("ERRO: Opção inválida!");
-                    opcao = 0;
+                    esperar(1500);
+                    opcao1 = 0;
             }
-            if (opcao == 7) {
-                System.out.println("Até mais!");
+            //Se o usuário tiver escolhido sair
+            if (opcao1 == 7) {
+                System.out.println("\n===========================\n\tTchauzinho!\n===========================\n");                
                 break;
             }
         } while (true);
     }
+    //Método que pausa a aplicação por x milisegundos (1000 ms = 1 s)
     public static void esperar(int ms) {
         try {
             Thread.sleep(ms);
