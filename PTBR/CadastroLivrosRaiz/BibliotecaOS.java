92e7efe01b1bfaf9a1510074c4645dd73d3cddab
Gabriel Silveira de Luna <gabriel.luna@lab.infnet.com.br>
2023-08-21 12:35:07 -0300

    CadastroLivrosProjeto (wip)

---
+++ PTBR/CadastroLivrosRaiz/BibliotecaOS.java
@@ -0,0 +1,150 @@
+package PTBR.CadastroLivrosRaiz;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BibliotecaOS {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nomeLivro = "", autor = "", editora = "", CDD = "", ISBN = "", secao = "";
+        int edicao = 0, anoPublicacao = 0, prateleira = 0, opcao = 0;
+        ArrayList<Livro> listaLivros = new ArrayList<Livro>();
+
+        
+        //(Nintendo) Switch
+        do {
+            switch (opcao) {
+                //Intro
+                case 0:      
+                    System.out.println("\n===================================\n\tCadastro De Livros\n\t\tv1.0\n===================================");
+                    System.out.println("\nBem-vindo(a), usuário(a)! Insira o número correspondente à opção desejada:");
+                    //Opções
+                    System.out.println("\n[1] Cadastrar\n[2] Procurar pro palavra-chave\n[3] Listar livros\n[4] Espaço livre/usado\n[5] Excluir último elemento\n[6] Ordenar livros cadastrados\n[7] Sair\n");
+                    //Input
+                    do {
+                        try {
+                            System.out.print("> ");
+                            opcao = scanner.nextInt();
+                            break;
+                        }
+                        catch(InputMismatchException e) {
+                            System.out.println("ERRO: Insira apenas números inteiros, por favor.");
+                            scanner.next();
+                        }
+                    } while (true);
+                    scanner.nextLine();
+                    break;
+                //Cadastro
+                case 1:
+                    if (listaLivros.size() < 1000) {
+                        //Nome
+                        do {
+                            System.out.print("Insira o nome do livro: ");
+                            nomeLivro = scanner.nextLine();
+                            if (!nomeLivro.trim().isEmpty()) {
+                                break;
+                            }
+                        } while(true);  
+                        //Autor
+                        do {
+                            System.out.print("Insira o Autor do livro: ");
+                            autor = scanner.next();
+                            if (!autor.trim().isEmpty()) {
+                                break;
+                            }
+                        } while(true); 
+                        //Editora
+                        do {
+                            System.out.print("Insira a editora do livro: ");
+                            editora = scanner.next();
+                            if (!editora.trim().isEmpty()) {
+                                break;
+                            }
+                        } while(true); 
+                        //CDD
+                        do {
+                            System.out.print("Insira o CDD do livro: ");
+                            CDD = scanner.next();
+                            if (!CDD.trim().isEmpty()) {
+                                break;
+                            }
+                        } while(true);
+                        //ISBN
+                        do {
+                            scanner.nextLine();
+                            System.out.print("Insira o ISBN do livro: ");
+                            ISBN = scanner.next();
+                            if (!ISBN.trim().isEmpty()) {
+                                break;
+                            }
+                        } while(true);
+                        //Edição
+                        do {
+                            try {
+                                System.out.print("Insira a edição do livro: ");
+                                edicao = scanner.nextInt();
+                                break;
+                            }
+                            catch (InputMismatchException e) {
+                                System.out.println("ERRO: Apenas números, por favor.");
+                            }
+                        } while(true);
+                        //Ano de Publicação
+                        do {
+                            try {
+                                System.out.print("Insira o ano de publicação do livro: ");
+                                anoPublicacao = scanner.nextInt();
+                                break;
+                            }
+                            catch (InputMismatchException e) {
+                                System.out.println("ERRO: Apenas números, por favor.");
+                            }
+                        } while(true);
+                        //Seção
+                        do {
+                            try {
+                                scanner.nextLine();
+                                System.out.print("Insira o nome da seção onde está o livro: ");
+                                secao = scanner.next();
+                                if (!secao.trim().isEmpty()) {
+                                break;
+                                }
+                            }
+                            catch (InputMismatchException e) {
+                                System.out.println("ERRO: Apenas números, por favor.");
+                            }
+                        } while(true);
+                        //Prateleira
+                        do {
+                            try {
+                                System.out.print("Insira a prateleira onde está o livro: ");
+                                prateleira = scanner.nextInt();
+                                break;
+                            }
+                            catch (InputMismatchException e) {
+                                System.out.println("ERRO: Apenas números, por favor.");
+                            }
+                        } while(true);
+                        //Adicionar à lista
+                        listaLivros.add(new Livro(nomeLivro, autor, editora, CDD, ISBN, edicao, anoPublicacao, secao, prateleira));
+                        opcao = 0;
+                    } else {
+                       System.out.println("ERRO: A lista está cheia!");
+                    }
+                    break;
+                case 3:
+                    if (listaLivros.size() == 0) {
+                        System.out.println("Não há nenhum livro cadastrado!");
+                    } else {
+                        for (int i = 0; i < listaLivros.size(); i++) {
+                            System.out.printf("==================================================\n\t\t[Livro %d#]\n\n Nome do Livro: %s\n%s\nAutor: %s\nEditora: %s\nCDD: %s\nISBN: %s\nEdição: %d°\nAno de Publicação: %d\nSeção: %s\nPrateleira: %d",i,nomeLivro,autor,editora,CDD,ISBN,edicao,anoPublicacao,secao,prateleira);
+                        }
+                        opcao = 0;
+                    }
+            }
+               
+            
+        } while (true);
+    }
+}
---
+++ PTBR/CadastroLivrosRaiz/Livro.java
@@ -0,0 +1,73 @@
+package PTBR.CadastroLivrosRaiz;
+
+public class Livro {
+    String nomeLivro, autor, editora, CDD, ISBN, secao;
+    int edicao, anoPublicacao, prateleira;
+    Livro(String nomeLivro, String autor, String editora, String CDD, String ISBN, int edicao, int anoPublicacao, String secao, int prateleira) {
+        this.nomeLivro = nomeLivro;
+        this.autor = autor;
+        this.editora = editora;
+        this.CDD = CDD;
+        this.ISBN = ISBN;
+        this.edicao = edicao;
+        this.anoPublicacao = anoPublicacao;
+        this.secao = secao;
+        this.prateleira = prateleira;
+    }
+    //GETTERS
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public String getEditora() {
+        return editora;
+    }
+    public String getCDD() {
+        return CDD;
+    }
+    public String getISBN() {
+        return ISBN;
+    }
+    public int getEdicao() {
+        return edicao;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    public String getSecao() {
+        return secao;
+    }
+    public int getPrateleira() {
+        return prateleira;
+    }
+    //SETTERS
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    public void setCDD(String CDD) {
+        this.CDD = CDD;
+    }
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+    public void setPrateleira(int prateleira) {
+        this.prateleira = prateleira;
+    }
+}
